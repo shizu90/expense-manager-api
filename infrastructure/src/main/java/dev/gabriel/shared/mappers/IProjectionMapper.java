79426565969742a23e9308cf1a302dc5a95aafcf
shizu90 <gabriel.moretto9090@gmail.com>
2024-01-09 14:53:18 -0300

    deleted aggregate root projection

--- infrastructure/src/main/java/dev/gabriel/shared/mappers/IProjectionMapper.java
+++
@@ -1,8 +0,0 @@
-package dev.gabriel.shared.mappers;
-
-import dev.gabriel.shared.models.AggregateRoot;
-import dev.gabriel.shared.projection.AggregateRootProjection;
-
-public interface IProjectionMapper<TProjection extends AggregateRootProjection, TEntity extends AggregateRoot> {
-    TEntity toDomain(TProjection tProjection);
-}
